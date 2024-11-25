f1a3ec15578d72dfed4133f177542f823b1f1c18
Shrijan274 <shrijan.lingam@clientservertech.com>
2024-11-25 22:10:52 +0530

    Clean code

--- django_generic_api/views.py
+++ django_generic_api/views.py
@@ -252,9 +252,12 @@ class GenericRegisterAPIView(APIView):
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
-        if not getattr(settings, 'BASE_URL', None):
+        if not getattr(settings, "BASE_URL", None):
             return Response(
-                {"error": "Configure BASE_URL before registration.", "code": "DGA-V023"},
+                {
+                    "error": "Configure BASE_URL before registration.",
+                    "code": "DGA-V023",
+                },
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
