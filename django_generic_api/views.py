5b89ae6e09a0d64eaee60e9b17c3c47792bb27c1
Shrijan274 <shrijan.lingam@clientservertech.com>
2024-12-21 04:40:12 +0530

    #63 1.Updated get_model_by_name() which accepts as appName.modelName as input

--- django_generic_api/views.py
+++ django_generic_api/views.py
@@ -73,9 +73,9 @@ class GenericSaveAPIView(APIView):
 
         try:
             model = get_model_by_name(model_name)
-        except ValueError:
+        except Exception as e:
             return Response(
-                {"error": "Model not found", "code": "DGA-V003"},
+                {"error": e.args[0]["error"], "code": e.args[0]["code"]},
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
@@ -138,11 +138,12 @@ class GenericFetchAPIView(APIView):
         # check if user has permission to view the data.
         try:
             model = get_model_by_name(model_name)
-        except ValueError:
+        except Exception as e:
             return Response(
-                {"error": "Model not found", "code": "DGA-V007"},
-                status=status.HTTP_404_NOT_FOUND,
+                {"error": e.args[0]["error"], "code": e.args[0]["code"]},
+                status=status.HTTP_400_BAD_REQUEST,
             )
+
         if not self.request.user.has_perm(make_permission_str(model, "fetch")):
             return Response(
                 {
